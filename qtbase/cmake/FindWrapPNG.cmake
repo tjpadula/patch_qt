8b23d06d40227d2d90ed12fda7a009755a5054fc
Tom Padula <tom@Toms-MacBook-Pro.local>
2025-04-16 15:24:09 -0700

    When building for qt6, we need one set of directories, when building for MS we need another.

--- qtbase/cmake/FindWrapPNG.cmake
+++ qtbase/cmake/FindWrapPNG.cmake
@@ -1,9 +1,25 @@
 if(IOS OR MACOS)
     set(FINDWRAP_DIR "${CMAKE_CURRENT_LIST_DIR}/../../qtbase/cmake")
     get_filename_component(FINDWRAP_DIR_ABSOLUTE ${FINDWRAP_DIR} ABSOLUTE)
-    #message (STATUS "FINDWRAP_DIR_ABSOLUTE: ${FINDWRAP_DIR_ABSOLUTE}")
+    message (STATUS "CMAKE_CURRENT_LIST_DIR: ${CMAKE_CURRENT_LIST_DIR}")
+    message (STATUS "FINDWRAP_DIR: ${FINDWRAP_DIR}")
+    message (STATUS "FINDWRAP_DIR_ABSOLUTE: ${FINDWRAP_DIR_ABSOLUTE}")
     include_directories("${FINDWRAP_DIR_ABSOLUTE}")
-    include("${FINDWRAP_DIR_ABSOLUTE}/QtFindWrapHelper.cmake" NO_POLICY_SCOPE)
+    
+    if(EXISTS "${FINDWRAP_DIR_ABSOLUTE}/QtFindWrapHelper.cmake")
+        include("${FINDWRAP_DIR_ABSOLUTE}/QtFindWrapHelper.cmake" NO_POLICY_SCOPE)
+    endif()
+    
+    set(FINDWRAP_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../../qtbase/cmake")
+    get_filename_component(FINDWRAP_DIR_ABSOLUTE ${FINDWRAP_DIR} ABSOLUTE)
+    message (STATUS "FINDWRAP_DIR: ${FINDWRAP_DIR}")
+    message (STATUS "FINDWRAP_DIR_ABSOLUTE: ${FINDWRAP_DIR_ABSOLUTE}")
+    include_directories("${FINDWRAP_DIR_ABSOLUTE}")
+
+    if(EXISTS "${FINDWRAP_DIR_ABSOLUTE}/QtFindWrapHelper.cmake")
+        include("${FINDWRAP_DIR_ABSOLUTE}/QtFindWrapHelper.cmake" NO_POLICY_SCOPE)
+    endif()
+    
 else(IOS OR MACOS)
     include(QtFindWrapHelper NO_POLICY_SCOPE)
 endif(IOS OR MACOS)
